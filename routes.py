331c872dd6536ed9c7ed5b676014aaf4af329323
rik-code <topelven1234567890@gmail.com>
2022-02-12 18:27:47 +0300

    Adding routing pages

--- routes.py
+++ routes.py
@@ -5,3 +5,13 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/add-student')
+def add_students():
+    return render_template('add_students.html')
+
+
+@app.route('/add-subject')
+def add_students():
+    return render_template('add_subject.html')
