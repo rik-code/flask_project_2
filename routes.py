68f8551a2e7b59516faa21098dd2515401deecd3
rik-code <topelven1234567890@gmail.com>
2022-03-26 18:54:01 +0300

    Adding login function

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for
 from app import app, db, login
-from forms import StudentForm, SubjectForm
+from forms import StudentForm, SubjectForm, LoginForm
 from models import Subjects, Students, User
 from flask_login import current_user, login_user
 
@@ -98,3 +98,11 @@ def delete_subject(id):
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
